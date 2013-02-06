aaaa8955abf640f8964c55f6e8d8b7e28b961576
chinmay <cdbapat92@gmail.com>
2013-02-06 22:01:46 +0530

    Added floor members

--- floor.c
+++ floor.c
@@ -1,7 +1,12 @@
 #include "floor.h"
 
-floor floor_new(){
+floor floor_new(int num){
     floor *ret = malloc(sizeof(floor));
     ret->up = queue_new();
     ret->down = queue_new();
+    ret->memnum = 0;
+    int i;
+    for(i = 0; i < FLOOR_CAP; i++){
+        ret->members[i] = NULL;
+    }
 }
--- floor.h
+++ floor.h
@@ -1,12 +1,18 @@
-#include "Queue.h"
+#include "queue.h"
 
 #ifndef FLOOR_H
 #define FLOOR_H
+
+#define FLOOR_CAP 20
 
 typedef struct floor_ floor;
 struct floor_{
     Queue* up;
     Queue* down;
+    Person* members[FLOOR_CAP];
+    int memnum;
 };
 
 floor floor_new();
+
+#endif
