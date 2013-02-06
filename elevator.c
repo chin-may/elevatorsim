ca9f1b21d2f6e0a5075ffc20f6f4b1196bff78ee
chinmay <cdbapat92@gmail.com>
2013-02-06 18:46:01 +0530

    Wrote elevator functions

---
+++ elevator.c
@@ -0,0 +1,119 @@
+#include "elevator.h"
+#include "consts.h"
+#include "queue.h"
+
+elevator* elevator_new(){
+    elevator* ret = malloc(sizeof(elevator));
+    int i;
+    for(i = 0; i < ELEVATOR_CAP; i++){
+        ret->passengers[i] = 0;
+    }
+    ret->pnum = 0;
+    ret->location = 0;
+    ret->delay = 0;
+    for(i = 0; i< FLOORNUM; i++){
+        ret->dest[i] = 0;
+    }
+    return ret;
+}
+
+void elevator_move(elevator* e){
+    if(e->delay){
+        e->delay--;
+    }
+    else{
+        e->location += e->moving;
+    }
+}
+
+
+
+void elevator_pause(elevator* e, int time){
+    e->delay = time;
+    int i;
+    //e->dest[e->location]  = 0;
+}
+
+int elevator_isfree(elevator* e){
+    int i;
+    for(i = 0; i < FLOORNUM; i++){
+        if(dest[i] != 0){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+
+void elevator_setdir(elevator* e){
+    int i;
+    int has_up = 0, has_down = 0;
+    for(i = 0; i < ELEVATOR_CAP; i++){
+        if(e->passengers[i] != NULL){
+            if(e->passengers[i]->dest > e->location){
+                has_up = 1;
+            }
+            else if(e->passengers[i]->dest < e->location){
+                has_down = 1;
+            }
+        }
+    }
+    if(e->moving == 0){
+        if(has_up) moving = 1;
+        else if(has_down) moving = -1;
+    }
+    else if(e->moving == 1){
+        if(has_up) moving = 1;
+        else if(has_down) moving = -1;
+        else moving = 0;
+    }
+    else{
+        if(has_down) moving = -1;
+        else if(has_up) moving = 1;
+        else moving = 0;
+    }
+}
+
+
+void elevator_setdest(elevator* e){
+    int i;
+    for(i = 0; i < FLOORNUM; i++){
+        e->dest[i] = 0;
+    }
+    for(i = 0; i < ELEVATOR_CAP; i++){
+        if(e->passengers[i] != NULL){
+            e->dest[e->passengers[i]->dest] = 1;
+        }
+    }
+}
+
+
+void elevator_enter(elevator* e, Person* p){
+    if(e->pnum == ELEVATOR_CAP){
+        perror("Elevator overload");
+        exit(1);
+    }
+    int i = 0;
+    while(e->passengers[i] != NULL){
+        i++
+    }
+    e->passengers[i] = p;
+    e->pnum++;
+}
+
+
+Queue* elevator_leave(elevator* e){
+    int i;
+    Queue* q = queue_new();
+    for(i = 0; i < ELEVATOR_CAP; i++){
+        if(e->passengers[i] != NULL){
+            if(e->passengers[i]->dest == e->location){
+                queue_enque(q, e->passengers[i]);
+                e->passengers[i] = NULL;
+                e->pnum--;
+            }
+
+        }
+    }
+    return q;
+}
--- elevator.h
+++ elevator.h
@@ -1,11 +1,37 @@
+#include "consts.h"
 #ifndef ELEVATOR_H
 
-#define ELEVATOR_CAP 8
 #define ELEVATOR_H
+#define ELEVATOR_CAP 8
+#define ELEVATOR_DELAY 3
 
 typedef struct elevator_ elevator;
 struct elevator_{
     Person* passengers[ELEVATOR_CAP];
     int pnum;
+    int location;
+    int delay;
+    int moving;
+    //int free;
+    int dest[FLOORNUM];
 }
+
+elevator* elevator_new();
+
+void elevator_move(elevator* e);
+
+//void elevator_stop(elevator* e);
+//
+void elevator_pause(elevator* e, int time);
+
+int elevator_isfree(elevator* e);
+
+void elevator_setdir(elevator* e);
+
+void elevator_setdest(elevator* e);
+
+void elevator_enter(elevator* e, Person* p);
+
+Queue* elevator_leave(elevator* e);
+
 #endif
